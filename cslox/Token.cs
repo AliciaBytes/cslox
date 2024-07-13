2df25582d55c8382e585052b204a6b7e0ff869e1
Alicia Jasmine Miedl <alicia@aliciabytes.com>
2024-07-13 22:14:07 +0200

    Implement Scanner
    See: https://craftinginterpreters.com/scanning.html

--- cslox/Token.cs
+++ cslox/Token.cs
@@ -6,10 +6,10 @@ namespace CSLox
     {
         readonly TokenType type;
         readonly string lexeme;
-        readonly Object literal;
+        readonly object? literal;
         readonly int line;
 
-        public Token(TokenType type, string lexeme, object literal, int line)
+        public Token(TokenType type, string lexeme, object? literal, int line)
         {
             this.type = type;
             this.lexeme = lexeme;
